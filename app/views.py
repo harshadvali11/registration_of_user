6e8e00d1b824cfd99b54f73d6e4cbcd5cea0d578
jspmern <jspmern@gmail.com>
2024-06-04 15:58:28 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -4,7 +4,20 @@ from django.shortcuts import render
 from django.core.mail import send_mail
 
 from app.forms import *
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
+
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+
+def home(request):
+    if request.session.get('username'):
+        username=request.session.get('username')
+        d={'username':username}
+        return render(request,'home.html',d)
+    return render(request,'home.html')
+
+
 def registration(request):
     EUFO=UserForm()
     EPFO=ProfileForm()
@@ -34,10 +47,35 @@ def registration(request):
         else:
             return HttpResponse('Invalid data')
 
-
-
-
-
-
-
     return render(request,'registration.html',d)
+
+
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST['un']
+        password=request.POST['pw']
+
+        AUO=authenticate(username=username,password=password)
+        if AUO and AUO.is_active:
+            login(request,AUO)
+            request.session['username']=username
+            return HttpResponseRedirect(reverse('home'))
+        else:
+            return HttpResponse('Invalid Credentials')
+
+
+
+
+
+
+
+    return render(request,'user_login.html')
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
+
+
+
